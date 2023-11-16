eb3e67f11c071c62bf2daee855a2d9e42fd02a4a
kistalex <kistalexander@yandex.ru>
2023-11-16 20:57:54 +0400

    Initial Commit

---
+++ TimeIsMoney/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//
+// TimeIsMoney
+// ViewController.swift
+// 
+// Created by Alexander Kist on 16.11.2023.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
