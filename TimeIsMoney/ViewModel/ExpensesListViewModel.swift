47e5cbd761631411d40e8aa5e14038c22ad37e5d
kistalex <kistalexander@yandex.ru>
2023-11-20 23:34:54 +0400

    Переработан UI, отображение таблицы с внесенными текущими тратами, в настройки добавлена кнопка удаления всех текущих трат

---
+++ TimeIsMoney/ViewModel/ExpensesListViewModel.swift
@@ -0,0 +1,10 @@
+//
+//
+// TimeIsMoney
+// ExpensesListViewModel.swift
+// 
+// Created by Alexander Kist on 19.11.2023.
+//
+
+
+import Foundation
---
+++ TimeIsMoney/Views/ExpensesList/HeaderView.swift
@@ -0,0 +1,10 @@
+//
+//
+// TimeIsMoney
+// HeaderView.swift
+// 
+// Created by Alexander Kist on 20.11.2023.
+//
+
+
+import Foundation
